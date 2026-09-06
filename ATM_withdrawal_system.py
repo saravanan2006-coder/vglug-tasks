c6c1b125e8eaa2452d5d4ca98de291c84a3b8aee
Saravanan <saravananshavan852.gmail.com>
2026-09-06 19:12:10 +0530

    modified atm withdrawal system and added shopping.py

--- ATM_withdrawal_system.py
+++ ATM_withdrawal_system.py
@@ -1,14 +1,12 @@
-#this is just a prototype of how atm works after the user insert their card
-
-print("Insert Your ATM card")
-pin = input("Enter your 4 digit PIN: ")
-if len(pin) == 4:
-    withdrawal_amt = int(input("Enter the amount to be Withdraw(amount must be multiple of 100,200,500): "))
-    if withdrawal_amt % 100 == 0:
-        print("your transaction completed , collect your ATM card then your cash")
+balance = 10000
+while balance > 0:
+    withdrawal_amount = int(input("Enter the withdrawal amount: "))
+    if withdrawal_amount <= balance:
+        balance = balance - withdrawal_amount
+        print("withdrawal successfull")
+        print("remaining balance: ",balance)
+    
     else:
-        print("enter a valid amount")
-else:
-    print("invalid pin")
-    print("collect your card and try again")
+        print("Insufficient balance")
 
+    
---
+++ shopping.py
@@ -0,0 +1,7 @@
+bill = 0
+for i in range(5):
+    price=int(input("Enter the price of an item: "))
+    bill = bill + price
+print("------------------------------------")
+print("Your Total bill amount is ",bill)
+    
